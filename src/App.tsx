1c760173f2566b07d2fd0e549817ca99128b410f
Zaranix <zahrakhodabakhshian54@gmail.com>
2023-12-01 20:21:33 +0100

    create expense table

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import ExpenseList from "./expenseList";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  const [expenses , setExpenses] = useState([
+   { description : "string",
+  amount : 2,
+  catergory : "string",
+  id : 2},
+  { description : "string",
+  amount : 2,
+  catergory : "string",
+  id : 4}
+  ])
+  
+  const deleteHandler = (id : number) => {
+    setExpenses(expenses.filter(expense => expense.id !== id))
+  console.log(id)};
+    if(expenses.length !== 0)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ExpenseList expenses={expenses} onDelete={deleteHandler} />
   )
 }
 
---
+++ src/expenseList.tsx
@@ -0,0 +1,50 @@
+
+interface Expense  {
+  description : string,
+  amount : number,
+  catergory : string,
+  id : number
+  }
+//send expenses as a list 
+interface Props {
+  expenses : Expense[]
+//because the list of expenses passed by props so we should update them in parent component so we need callback function
+  onDelete : (id: number) => void
+}
+  const ExpenseList = ({ expenses , onDelete } : Props) => {
+  
+  console.log(expenses)
+    return (
+      <>
+      <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map(expense => <tr key={expense.id}>
+          <td>{expense.description}</td>
+          <td>{expense.amount}</td>
+          <td>{expense.catergory}</td>
+          <td><button className="btn btn-outline-danger" onClick={() => onDelete(expense.id)} >delete</button></td></tr>)}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>{expenses.reduce((acc , expense) => acc + expense.amount , 0).toFixed(2)}</td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
+      </table>
+      </>
+  
+  
+    )
+  }
+  
+  export default ExpenseList
